d3d0e01982986f182cd99fd272a016e4a6d8016a
sojoudian <sojodyan@gmail.com>
2022-01-05 21:57:50 -0500

    added health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHttp "github.com/sojoudian/goRestfulAPI/internal/transport/http"
+)
 
 // App - the struct which contains things like pointers
 // to database connections
@@ -11,6 +16,15 @@ type App struct {
 //Run - sets up our application
 func (app *App) Run() error {
 	fmt.Println("Setting up our APP")
+
+	handler := transportHttp.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
+
 	return nil
 }
 func main() {
--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -1,9 +1,15 @@
 package http
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 //Handler - stores pointers to our comment service
 type Handler struct {
+	Router *mux.Router
 }
 
 //function is in capital letter because we want to call it from our main function
@@ -16,4 +22,8 @@ func NewHandler() *Handler {
 //SetupRoutes - sets up all the routes for our application
 func (h *Handler) SetupRoutes() {
 	fmt.Println("Setting up Routes")
+	h.Router = mux.NewRouter()
+	h.Router.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "I am alive!")
+	})
 }
