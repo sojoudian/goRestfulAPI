24e6f5a3fbbf12dddd95ff04dfc368c89e33e0d5
sojoudian <sojodyan@gmail.com>
2022-01-06 19:32:06 -0500

    add database.go, create a connection with postgres DB

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/sojoudian/goRestfulAPI
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/lib/pq v1.1.1 // indirect
+)
---
+++ internal/database/database.go
@@ -0,0 +1,32 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	- "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+//NewDatabase - returns a pointer to a database object
+func NewDatabase() (*grom.DB, err) {
+	fmt.Println("Set up new database connection")
+
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbTable := os.Getenv("DB_TABLE")
+	dbPort := os.Getenv("DB_PORT")
+
+	connectionString := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", dbHost, dbPort, dbUsername, dbTable, dbPassword)
+
+	db, err := gorm.Open("postgres", connectionString)
+	if err != nil {
+		return db, err
+	}
+	if err := db.DB().Ping(); err != nil {
+		return db, err
+	}
+
+	return db, nil
+}
