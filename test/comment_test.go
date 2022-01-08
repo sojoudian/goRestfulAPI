b570bb6f70ec3401e8b6f8196a52444eb2c9e202
sojoudian <sojodyan@gmail.com>
2022-01-07 20:46:32 -0500

    update test GetComments endpoint

---
+++ test/comment_test.go
@@ -0,0 +1,19 @@
+// +build e2e
+
+package test
+
+import (
+	"github.com/go-resty/resty/v2"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestGetComments(t *testing.T) {
+	client := resty.New()
+	resp, err := client.R().Get(BASE_URL + "/api/comment")
+	if err != nil {
+		t.Fail()
+	}
+
+	assert.Equal(t, 200, resp.StatusCode())
+}
