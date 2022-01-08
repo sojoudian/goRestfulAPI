d36b5585ea187d00e38ab1dae3fe3d9449e14e87
sojoudian <sojodyan@gmail.com>
2022-01-07 20:52:07 -0500

    update test PostComments endpoint

--- test/comment_test.go
+++ test/comment_test.go
@@ -1,3 +1,4 @@
+//go:build e2e
 // +build e2e
 
 package test
@@ -15,5 +16,14 @@ func TestGetComments(t *testing.T) {
 		t.Fail()
 	}
 
+	assert.Equal(t, 200, resp.StatusCode())
+}
+
+func TestPostComment(t *testing.T) {
+	client := resty.New()
+	resp, err := client.R().
+		SetBody(`{"slug": "/", "author": "testAuthor", "body": "This is a test comment"}`).
+		Post(BASE_URL + "/api/comment")
+	assert.NoError(t, err)
 	assert.Equal(t, 200, resp.StatusCode())
 }
